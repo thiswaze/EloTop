9cec4ba6b08769d8b99e31a96a5b27490823df57
thiswaze <huseyinfsumer@gmail.com>
2026-03-07 21:00:54 +0300

    Add constructor to GUIClickListener
    Added a constructor to initialize the plugin field.

--- src/main/java/com/elotop/listener/GUIClickListener.java
+++ src/main/java/com/elotop/listener/GUIClickListener.java
@@ -4,7 +4,9 @@ import com.elotop.EloTopPlugin;
 import org.bukkit.event.Listener;
 
 public class GUIClickListener implements Listener {
+
     private final EloTopPlugin plugin;
+
     public GUIClickListener(EloTopPlugin plugin) {
         this.plugin = plugin;
     }
